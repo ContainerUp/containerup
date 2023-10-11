b734c0aaf54cc2be6092a3ecc8798ef82217163d
yylyyl <yangyiliang@gmail.com>
2023-10-11 17:07:10 -0400

    Rename env variable in Containerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [[ -z "$PASSWORD" ]]; then
-    echo "Environment variable PASSWORD is required."
+if [[ -z "$CONTAINERUP_PASSWORD" ]]; then
+    echo "Environment variable CONTAINERUP_PASSWORD is required."
     echo "Generate one by command 'echo <username>:<password> | sha256sum'."
     exit 1
 fi
 
-exec /usr/bin/containerup -listen 0.0.0.0:3876 -password "$PASSWORD"
+exec /usr/bin/containerup -listen 0.0.0.0:3876 -password "$CONTAINERUP_PASSWORD"
