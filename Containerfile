3aabea23fcc4b94d3dc11fe719ff2a5769b7a999
yylyyl <yangyiliang@gmail.com>
2023-10-12 14:53:11 -0400

    reduce binary size

--- Containerfile
+++ Containerfile
@@ -1,5 +1,5 @@
-FROM docker.io/library/debian:12
-RUN apt-get update && apt-get install -y media-types && apt-get clean
+FROM alpine:3.18
+COPY mime.types /etc
 COPY containerup /usr/bin
 COPY entrypoint.sh /
 EXPOSE 3876
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 if [[ -z "$CONTAINERUP_PASSWORD" ]]; then
     echo "Environment variable CONTAINERUP_PASSWORD is required."
     echo "Generate one by command 'echo <username>:<password> | sha256sum'."
     exit 1
 fi
 
-exec /usr/bin/containerup -listen 0.0.0.0:3876 -password "$CONTAINERUP_PASSWORD"
+V3=""
+if [[ -n "$CONTAINERUP_PODMAN_V3" ]]; then
+    V3="-v3"
+fi
+
+exec /usr/bin/containerup -listen 0.0.0.0:3876 -password "$CONTAINERUP_PASSWORD" $V3
